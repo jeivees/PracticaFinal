85d2caafbe59d9b5586031324408ffb655e06205
jeivees <157594955+jeivees@users.noreply.github.com>
2024-04-10 12:14:23 +0200

    Add files via upload

---
+++ Simulacion de vida/src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/simulaciondevida/HelloController.java
@@ -0,0 +1,14 @@
+package es.uah.matcomp.mp.teoria.gui.mvc.javafx.simulaciondevida;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
