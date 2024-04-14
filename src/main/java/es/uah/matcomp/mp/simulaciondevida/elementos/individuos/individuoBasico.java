9802f6ff710b83bcd5b1571b6ab6f3d5ca09dc40
jaizu23 <157589155+jaizu23@users.noreply.github.com>
2024-04-14 20:13:31 +0200

    Añadidos metodos de mover individuos

--- src/main/java/es/uah/matcomp/mp/simulaciondevida/elementos/individuos/individuoBasico.java
+++ src/main/java/es/uah/matcomp/mp/simulaciondevida/elementos/individuos/individuoBasico.java
@@ -1,5 +1,7 @@
 package es.uah.matcomp.mp.simulaciondevida.elementos.individuos;
 
+import java.util.Random;
+
 public class individuoBasico extends individuoAbstract {
     public individuoBasico(int I, int G, int T, float PR, float PC) {
         super(I, G, T, PR, PC);
@@ -8,5 +10,20 @@ public class individuoBasico extends individuoAbstract {
     @Override
     public String getTipo () {
         return "individuoBasico";
+    }
+
+    @Override
+    public void mover() {
+        Random r = new Random();
+        int movimiento = r.nextInt(4);
+        if (movimiento == 1) {
+            this.setPosicionX(this.getPosicionX() - 1);
+        } else if (movimiento == 2) {
+            this.setPosicionX(this.getPosicionX() + 1);
+        } else if (movimiento == 3) {
+            this.setPosicionY(this.getPosicionY() - 1);
+        } else {
+            this.setPosicionY(this.getPosicionY() + 1);
+        }
     }
 }
